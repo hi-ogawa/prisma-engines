e617c486a3506845d60c4b9f2bee77b226998acd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-09-01 17:28:48 +0900

    feat: expose `diff_schema` via napi

--- schema-engine/core/packages/diff-napi/README.md
+++ schema-engine/core/packages/diff-napi/README.md
@@ -7,5 +7,30 @@ pnpm build --release --strip
 
 ```js
 const lib = require("./index.js");
-lib.sum(1, 2);
+const sql = await lib.diffSchema(
+  `
+    model counter {
+        id    Int @id @default(autoincrement())
+    }
+  `,
+  `
+    model counter {
+        id    Int @id @default(autoincrement())
+        value Int
+    }
+  `,
+);
+console.log(sql);
+// -- AlterTable
+// ALTER TABLE "counter" ADD COLUMN     "value" INTEGER NOT NULL;
 ```
+
+## todo
+
+- choose features to reduce binary size?
+- is wasm-bindgen build possible since no IO needed for raw prisma schema diff feature?
+- frontend? (prisma.schema editor + generate raw sql for migration)
+
+## links
+
+- https://github.com/napi-rs/napi-rs/issues/796
--- schema-engine/core/packages/diff-napi/src/lib.rs
+++ schema-engine/core/packages/diff-napi/src/lib.rs
@@ -1,9 +1,35 @@
-#![deny(clippy::all)]
+use std::sync::Arc;
+
+use napi::Error;
+use psl::SourceFile;
+use schema_connector::SchemaConnector;
+use sql_schema_connector::SqlSchemaConnector;
 
 #[macro_use]
 extern crate napi_derive;
 
 #[napi]
-pub fn sum(a: i32, b: i32) -> i32 {
-    a + b
+pub async fn diff_schema(schema_string_from: String, schema_string_to: String) -> Result<String, Error> {
+    // TODO: ability to choose differnt connector
+    let mut connector = SqlSchemaConnector::new_postgres();
+
+    let schema_from = connector
+        .database_schema_from_diff_target(diff_target_from_schema_string(&schema_string_from), None, None)
+        .await
+        .map_err(|e| Error::from_reason(e.to_string()))?;
+
+    let schema_to = connector
+        .database_schema_from_diff_target(diff_target_from_schema_string(&schema_string_to), None, None)
+        .await
+        .map_err(|e| Error::from_reason(e.to_string()))?;
+
+    let migration = connector.diff(schema_from, schema_to);
+    let script_string = connector
+        .render_script(&migration, &Default::default())
+        .map_err(|e| Error::from_reason(e.to_string()))?;
+    Ok(script_string)
+}
+
+fn diff_target_from_schema_string(input: &str) -> schema_connector::DiffTarget {
+    schema_connector::DiffTarget::Datamodel(SourceFile::new_allocated(Arc::from(input)))
 }
