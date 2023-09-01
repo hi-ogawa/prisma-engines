c51063dbd3102d2e8134f7e8cff9b27c652f5c3a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-09-01 14:41:44 +0900

    chore: add TODO

--- schema-engine/core/examples/dev.rs
+++ schema-engine/core/examples/dev.rs
@@ -2,27 +2,34 @@ use std::sync::Arc;
 
 use psl::SourceFile;
 use schema_connector::SchemaConnector;
-use schema_core::{
-    commands,
-    json_rpc::types::{DiffParams, DiffTarget},
-    // schema_to_connector_unchecked,
-};
+// use schema_core::{
+//     commands,
+//     json_rpc::types::{DiffParams, DiffTarget},
+//     // schema_to_connector_unchecked,
+// };
 use sql_schema_connector::SqlSchemaConnector;
 
+// cargo run --release -p schema-core --example dev
 // cargo run -p schema-core --example dev
+
+// TODO:
+// - reduce features?
+// - napi build as a simple schema diff library (without IO (fs or db))?
+// - is wasm-bindgen build possible since no IO needed for raw prisma schema diff feature?
+// - frontend? (prisma.schema editor? generate raw sql for migration?)
 
 #[tokio::main]
 async fn main() {
     // diff via "commands"
-    let diff_params = DiffParams {
-        from: DiffTarget::Empty,
-        to: DiffTarget::Empty,
-        shadow_database_url: None,
-        script: false,
-        exit_code: None,
-    };
-    let result = commands::diff(diff_params, Arc::new(schema_connector::EmptyHost)).await;
-    dbg!(&result);
+    // let diff_params = DiffParams {
+    //     from: DiffTarget::Empty,
+    //     to: DiffTarget::Empty,
+    //     shadow_database_url: None,
+    //     script: false,
+    //     exit_code: None,
+    // };
+    // let result = commands::diff(diff_params, Arc::new(schema_connector::EmptyHost)).await;
+    // dbg!(&result);
 
     // get connector via schema
     // let schema_str = r#"
@@ -63,10 +70,10 @@ async fn main() {
     let schema_from = connector.empty_database_schema();
     let migration = connector.diff(schema_from, schema_to);
     let script_string = connector.render_script(&migration, &Default::default());
-    dbg!(&script_string);
 
     if let Ok(script) = script_string {
-        println!("** script **");
         println!("{}", script);
+    } else {
+        dbg!(&script_string);
     }
 }
