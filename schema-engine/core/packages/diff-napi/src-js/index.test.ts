ca2cf72f817d2099893cbe675e2ef648d8218032
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-09-02 14:02:50 +0900

    refactor: async to sync internally

--- schema-engine/core/packages/diff-napi/src-js/index.test.ts
+++ schema-engine/core/packages/diff-napi/src-js/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-const { diffSchema } = require("../index.js");
+const { diffSchema, diffSchema2 } = require("../index.js");
 
 describe(diffSchema, () => {
   it("basic", async () => {
@@ -20,6 +20,33 @@ describe(diffSchema, () => {
       "
     `);
     expect(diffSchema("mysql", schemaFrom, schemaTo)).toMatchInlineSnapshot(`
+      "-- AlterTable
+      ALTER TABLE \`counter\` ADD COLUMN \`value\` INTEGER NOT NULL;
+      "
+    `);
+  });
+});
+
+describe(diffSchema2, () => {
+  it("basic", async () => {
+    const schemaFrom = `
+      model counter {
+          id    Int @id @default(autoincrement())
+      }
+    `;
+    const schemaTo = `
+      model counter {
+          id    Int @id @default(autoincrement())
+          value Int
+      }
+    `;
+    expect(diffSchema2("postgres", schemaFrom, schemaTo))
+      .toMatchInlineSnapshot(`
+      "-- AlterTable
+      ALTER TABLE \\"counter\\" ADD COLUMN     \\"value\\" INTEGER NOT NULL;
+      "
+    `);
+    expect(diffSchema2("mysql", schemaFrom, schemaTo)).toMatchInlineSnapshot(`
       "-- AlterTable
       ALTER TABLE \`counter\` ADD COLUMN \`value\` INTEGER NOT NULL;
       "
--- schema-engine/core/packages/diff-napi/src/lib.rs
+++ schema-engine/core/packages/diff-napi/src/lib.rs
@@ -1,6 +1,7 @@
-use std::str::FromStr;
+use std::{str::FromStr, sync::Arc};
 
 use napi::Error;
+use psl::SourceFile;
 use psl_core::datamodel_connector::Flavour;
 use schema_connector::SchemaConnector;
 use sql_schema_connector::SqlSchemaConnector;
@@ -38,4 +39,55 @@ pub fn diff_schema(
         .render_script(&migration, &Default::default())
         .map_err(|e| Error::from_reason(e.to_string()))?;
     Ok(script_string)
+}
+
+#[napi]
+pub fn diff_schema2(
+    flavor_string: String,
+    schema_string_from: String,
+    schema_string_to: String,
+) -> Result<String, Error> {
+    // instantiate connector
+    let flavor = Flavour::from_str(&flavor_string).map_err(Error::from_reason)?;
+    let mut connector = match flavor {
+        Flavour::Cockroach => Ok(SqlSchemaConnector::new_cockroach()),
+        Flavour::Mongo => Err(Error::from_reason("Unsupported flavor")),
+        Flavour::Mysql => Ok(SqlSchemaConnector::new_mysql()),
+        Flavour::Postgres => Ok(SqlSchemaConnector::new_postgres()),
+        Flavour::Sqlite => Ok(SqlSchemaConnector::new_sqlite()),
+        Flavour::Sqlserver => Ok(SqlSchemaConnector::new_mssql()),
+    }?;
+
+    // use direct tokio block_on since napi::bindgen_prelude::block_on seems to have wrong typing
+    let async_rt = tokio::runtime::Builder::new_current_thread()
+        .build()
+        .map_err(|e| Error::from_reason(e.to_string()))?;
+
+    // parse prisma schema
+    let schema_from = async_rt
+        .block_on(connector.database_schema_from_diff_target(
+            diff_target_from_schema_string(&schema_string_from),
+            None,
+            None,
+        ))
+        .map_err(|e| Error::from_reason(e.to_string()))?;
+
+    let schema_to = async_rt
+        .block_on(connector.database_schema_from_diff_target(
+            diff_target_from_schema_string(&schema_string_to),
+            None,
+            None,
+        ))
+        .map_err(|e| Error::from_reason(e.to_string()))?;
+
+    // compute diff and render sql
+    let migration = connector.diff(schema_from, schema_to);
+    let script_string = connector
+        .render_script(&migration, &Default::default())
+        .map_err(|e| Error::from_reason(e.to_string()))?;
+    Ok(script_string)
+}
+
+fn diff_target_from_schema_string(input: &str) -> schema_connector::DiffTarget {
+    schema_connector::DiffTarget::Datamodel(SourceFile::new_allocated(Arc::from(input)))
 }
